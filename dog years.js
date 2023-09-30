c11449492788fe82c719b8266d1700787075e940
pauwan-github <gatambiaw@gmail.com>
2023-09-30 16:42:33 +0300

    Converts Human Years to dog years

---
+++ dog years.js
@@ -0,0 +1,14 @@
+// create a variable to store age and assigns value
+let Myage = 21;
+// create a variable and sets its value
+const earlyYears = 2 * 10.5;
+// calculates later years
+let laterYears = (Myage - 2) * 4;
+// calculate the final age in dogs years
+let myAgeinDogyears = earlyYears + laterYears;
+// creates a variable to store my name
+let myName ="Paul";
+//writes my name in capslock
+myName=myName.toLowerCase();
+// it prints out a sentence showinng my name, human age and the same human age in dog years
+console.log(`My name is ${myName}. I am ${Myage} in HUMAN AGE which is ${myAgeinDogyears} old in dog years`);
